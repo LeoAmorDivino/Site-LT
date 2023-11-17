624d44f4cf879e916084040d2bf53b79c42dca69
Leandro <134524532+LeoAmorDivino@users.noreply.github.com>
2023-11-17 15:14:00 -0300

    17/11
    Agora com conexão ao banco e com handlebar

--- index.js
+++ index.js
@@ -4,7 +4,33 @@ let app = express()
 let mysql = require('mysql2') // Se comunicar com o MySQL
 let conection = mysql.connect({ // Abrindo uma conexão com o MySQL
     user: 'root',
-    password: 'root',
+    password: 'ifba',
     host: 'localhost',
     database: 'hospital'
 })
+
+// View engine Handlebars
+let expressHandlebars = require('express-handlebars')
+
+app.engine('handlebars', expressHandlebars.engine({
+    defaultlayout: 'main'
+}))
+
+app.set('view engine', 'handlebars');
+
+app.use('/',
+    express.static(__dirname + '/public')
+)
+
+app.get('/', (req, res) => { // A rota raíz retorna a tela de menu
+    conection.query( // Listando dados
+    'select * from especialidades', (err, result) => {
+        res.render("especialidades", {especialidades: result})
+    })
+})
+
+app.listen(80, () => {
+    console.log("Servidor iniciado...")
+})
+
+
